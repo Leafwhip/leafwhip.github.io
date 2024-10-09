90b56fc735b180ac7bc740d2847a9883ad6bd6d3
Leafwhip <fallenlondon.com@gmail.com>
2024-10-09 15:59:32 -0700

    added gtb helper

---
+++ gtb/gtb.js
@@ -0,0 +1,114 @@
+let list = ["empty"];
+let wordboxList = [];
+for(let i=0;i<wordlist.length;i++){
+    let container = document.createElement("span");
+    let box = document.createElement("input");
+    box.type="checkbox";
+    box.class="wordbox";
+    box.dataset.id=i;
+    container.innerHTML=wordlist[i].split(',')[0].split(' ').map(a=>a.length).join(' ');
+    container.append(box);
+    wordboxContainer.append(container);
+    wordboxList.push(box);
+}
+let updateWordList = () => {
+    list = [];
+    if(themes.value.length){
+        list = themes.value.split(comma.checked ? "," : " ")
+    }
+    else{
+        for(let box of wordboxList){
+            if(box.checked){
+                list.push(wordlist[box.dataset.id].split(','));
+            }
+        }
+        list = list.flat();
+    }
+    if(!list.length){
+        list = ["empty"];
+    }
+}
+themes.addEventListener("input", updateWordList);
+comma.addEventListener("input", updateWordList);
+for(let box of wordboxList){
+    box.addEventListener("input", updateWordList);
+}
+let selected = "";
+let clue = "";
+let listmode = false;
+let listmodeAnswers = [];
+let listmodeAccepted = [];
+let updateClue = () => {
+    theme.innerHTML=clue.split("").join("&nbsp;").replace(" ","\t");;
+    if(listmode){
+        listmodeAnswers=[];
+        for(let i=0;i<list.length;i++){
+            if(clue.length==list[i].length){
+                let match = true;
+                for(let j=0;j<clue.length;j++){
+                    if(clue[j]!="_"&&clue[j]!=list[i][j]){
+                        match = false;
+                    }
+                }
+                if(match){
+                    listmodeAnswers.push(list[i]);
+                }
+            }
+        }
+        fraction.innerHTML = "0/" + listmodeAnswers.length;
+        accepted.innerHTML="";
+        listmodeAccepted=[];
+    }
+    
+}
+random.onclick = () => {
+    selected = list[Math.floor(Math.random() * list.length)];
+    clue=selected.replace(/[^ ]/g, "_")
+    listmode = listMode.checked;
+    updateClue();
+    result.innerHTML = "";
+};
+reveal.onclick = () => {
+    let n = Math.floor(Math.random() * clue.split("").reduce((a,b)=>a+(b=="_"),0));
+    for(let i=0;i<clue.length;i++){
+        if(clue[i]=="_"){
+            if(n==0){
+                clue=clue.split("");
+                clue[i]=selected[i];
+                clue=clue.join("");
+            }
+            n--;
+        }
+    }
+    updateClue();
+}
+revealRest.onclick = () => {
+    accepted.innerHTML=listmodeAnswers.join(", ");
+}
+alloff.onclick = () => {
+    for(let box of wordboxList) {
+        box.checked = false;
+    }
+    updateWordList();
+}
+allon.onclick = () => {
+    for(let box of wordboxList) {
+        box.checked = true;
+    }
+    updateWordList();
+}
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (!listmode){
+        result.innerHTML = guess.value == selected ? "you got it!" : "nope, try again :(";
+    }
+    else{
+        if(listmodeAnswers.includes(guess.value)&&!listmodeAccepted.includes(guess.value)){
+            listmodeAccepted.push(guess.value);
+            accepted.innerHTML=listmodeAccepted.join(", ");
+            fraction.innerHTML = listmodeAccepted.length + "/" + listmodeAnswers.length;
+
+        }
+    }
+    guess.value = "";
+})
